945f070a4eb67d3f7c9c169a35b23abeda5b0e3e
Loyane Menezes <loyanesezenem5@gmail.com>
2025-08-28 20:17:38 -0300

    fix(candidate): handle user already exists exception and update repository/controller

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package com.gestao.vagas.gestao_vagas.modules.candidate.controllers;
 
+import com.gestao.vagas.gestao_vagas.exceptions.UserFoundException;
 import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateEntity;
 import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -19,6 +20,11 @@ public class CandidateController {
 
     @PostMapping("/")
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
         return this.candidateRepository.save(candidateEntity);
     }
 }
