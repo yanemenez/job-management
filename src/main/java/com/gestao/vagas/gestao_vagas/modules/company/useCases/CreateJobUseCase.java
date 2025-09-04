e44177d9726437b246b7e0db45b7b30a67213670
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-03 21:59:44 -0300

    feat(company): add JobController, JobRepository and CreateJobUseCase

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,18 @@
+package com.gestao.vagas.gestao_vagas.modules.company.useCases;
+
+import com.gestao.vagas.gestao_vagas.modules.company.entities.JobEntity;
+import com.gestao.vagas.gestao_vagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateJobUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity) {
+        return this.jobRepository.save(jobEntity);
+    }
+
+}
