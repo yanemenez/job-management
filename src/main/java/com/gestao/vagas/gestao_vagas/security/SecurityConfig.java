aa1480b53dafa2432eac508d62d9b9079d65e7e1
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-04 23:40:11 -0300

    feat : add security configuration

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.gestao.vagas.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll();
+
+                    auth.anyRequest().authenticated();
+                });
+        return http.build();
+    }
+}
+
