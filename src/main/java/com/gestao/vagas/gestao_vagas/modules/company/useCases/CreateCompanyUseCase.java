80508d260d9190ee7ab00bbea87130a7c6f8f98c
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-09 23:19:21 -0300

    feat(company): update create company use case and repository

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import com.gestao.vagas.gestao_vagas.exceptions.UserFoundException;
 import com.gestao.vagas.gestao_vagas.modules.company.entities.CompanyEntity;
 import com.gestao.vagas.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +13,20 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
 
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
-        .ifPresent((user) -> {
-            throw new UserFoundException();
-        });
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
+
 }
