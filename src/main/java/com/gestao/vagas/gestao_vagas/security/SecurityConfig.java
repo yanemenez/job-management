9cb34041cc285f0c62338dcc169e8f66779d2eee
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-09 23:04:25 -0300

    chore(security): allow public access to /auth/company/ endpoint

--- src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.gestao.vagas.gestao_vagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,14 +12,20 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable()) // desabilita CSRF
                 .authorizeHttpRequests(auth -> {
+                    // mantém exatamente os nomes que você mandou
                     auth.requestMatchers("/candidate/").permitAll()
-                            .requestMatchers("/company/").permitAll();
-
-                    auth.anyRequest().authenticated();
+                            .requestMatchers("/company/").permitAll()
+                            .requestMatchers("/auth/company/").permitAll();
+                    auth.anyRequest().authenticated(); // resto precisa de autenticação
                 });
+
         return http.build();
     }
-}
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
