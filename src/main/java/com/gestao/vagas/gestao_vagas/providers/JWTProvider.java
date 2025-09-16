5d905babd1a9f88ec6678db6bd57b4b0d7ee17bf
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-16 18:27:44 -0300

    feat(JWT): inject secret key with @Value

--- src/main/java/com/gestao/vagas/gestao_vagas/providers/JWTProvider.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/providers/JWTProvider.java
@@ -3,15 +3,17 @@ package com.gestao.vagas.gestao_vagas.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JWTProvider {
 
+    @Value("${security.token.secret}")
     private String secretKey;
 
     public String validateToken(String token) {
-        token = token.replace("Bearer", "");
+        token = token.replace("Bearer ", "");
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
