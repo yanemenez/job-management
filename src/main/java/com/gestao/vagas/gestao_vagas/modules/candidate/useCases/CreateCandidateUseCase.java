be2c9faaf46b660b9d7a4d11f8a569fd6d805ead
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-21 16:11:11 -0300

    feat(candidate): encode password in CreateCandidateUseCase

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.gestao.vagas.gestao_vagas.exceptions.UserFoundException;
 import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateEntity;
 import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,19 @@ public class CreateCandidateUseCase {
     @Autowired //Manage my repository
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
 
