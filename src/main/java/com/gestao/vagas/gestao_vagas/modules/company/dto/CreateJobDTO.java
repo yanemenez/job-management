b79d95c8cda3261f88952d4e1d5e3b0d5b7b5065
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-21 16:01:47 -0300

    chore(dto): complete CreateJobDTO implementation

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,4 +1,11 @@
 package com.gestao.vagas.gestao_vagas.modules.company.dto;
 
+import lombok.Data;
+
+@Data
 public class CreateJobDTO {
+
+    private String description;
+    private String benefits;
+    private String level;
 }
