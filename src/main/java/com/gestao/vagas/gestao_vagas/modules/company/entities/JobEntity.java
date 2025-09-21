aca4be33c35ef049701c98415c40dea27837ae47
Loyane Menezes <loyanesezenem5@gmail.com>
2025-09-21 15:58:26 -0300

    refactor(job): add Lombok builder and constructors to JobEntity

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/entities/JobEntity.java
@@ -2,7 +2,10 @@ package com.gestao.vagas.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -10,6 +13,9 @@ import java.util.UUID;
 
 @Entity(name = "job")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
     @Id
