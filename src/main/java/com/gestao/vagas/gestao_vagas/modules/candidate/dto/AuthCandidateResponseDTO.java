f5bfb097bb765bf79a2eeeb57b7e27baad4babe5
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-12 20:38:40 -0300

    feat(auth): add expires_in field to token DTO

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -12,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class AuthCandidateResponseDTO {
 
     private String access_token;
+    private Long expires_in;
 }
