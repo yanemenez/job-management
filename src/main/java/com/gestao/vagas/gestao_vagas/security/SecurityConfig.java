de98bd994dfa9f52c54c10ebeda52b6c487987bd
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-12 20:29:54 -0300

    feat(security): allow public access to /candidate/auth endpoint

--- src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig {
                     // mantém exatamente os nomes que você mandou
                     auth.requestMatchers("/candidate/").permitAll()
                             .requestMatchers("/company/").permitAll()
-                            .requestMatchers("/auth/company").permitAll();
+                            .requestMatchers("/auth/company").permitAll()
+                            .requestMatchers("/candidate/auth").permitAll();
                     auth.anyRequest().authenticated(); // resto precisa de autenticação
                 })
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
