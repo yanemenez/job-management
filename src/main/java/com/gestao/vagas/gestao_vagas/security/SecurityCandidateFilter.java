e80c52176d1780e2f81ffebe5479fd8fb8cb25b6
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-12 22:10:08 -0300

    feat(security): implement SecurityCandidateFilter to validate JWT tokens

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityCandidateFilter.java
@@ -0,0 +1,42 @@
+package com.gestao.vagas.gestao_vagas.security;
+
+import com.gestao.vagas.gestao_vagas.providers.JWTCandidateProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityCandidateFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTCandidateProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        String header = request.getHeader("Authorization");
+
+        if (header != null) {
+            var token = this.jwtProvider.validateToken(header);
+
+            if (token == null) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            request.setAttribute("candidate_id", token.getSubject());
+            System.out.println(" ===== TOKEN =====");
+            System.out.println(token);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
