637598e97c4e593b0734d585556495f069efbf7f
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-16 20:32:33 -0300

    feat(security): add condition to handle /company routes

--- src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityFilter.java
@@ -25,20 +25,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        SecurityContextHolder.getContext().setAuthentication(null);
+        //SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
-        if (header != null) {
-            var subjectToken = this.jwtProvider.validateToken(header);
-            if (subjectToken.isEmpty()) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
+        if(request.getRequestURI().startsWith("/company")) {
+            if (header != null) {
+                var subjectToken = this.jwtProvider.validateToken(header);
+                if (subjectToken.isEmpty()) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
 
-            request.setAttribute("company_id", subjectToken);
-            UsernamePasswordAuthenticationToken auth =
-                    new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
-            SecurityContextHolder.getContext().setAuthentication(auth);
+                request.setAttribute("company_id", subjectToken);
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
         }
 
         filterChain.doFilter(request, response);
