b55bb1a48c1ed6dc5c3f389ab798aeff398d9341
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-16 20:43:17 -0300

    feat(security): implement role mapping and authentication for candidate routes

--- src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityCandidateFilter.java
@@ -6,11 +6,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 
 @Component
 public class SecurityCandidateFilter extends OncePerRequestFilter {
@@ -21,20 +24,32 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        SecurityContextHolder.getContext().setAuthentication(null);
+
+        //SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
-        if (header != null) {
-            var token = this.jwtProvider.validateToken(header);
+        if (request.getRequestURI().startsWith("/candidate")) {
+            if (header != null) {
+                var token = this.jwtProvider.validateToken(header);
 
-            if (token == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+                if (token == null) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
+
+                request.setAttribute("candidate_id", token.getSubject());
+                var roles = token.getClaim("roles").asList(Object.class);
+
+                var grants = roles.stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
+                        .toList();
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(),
+                    null,
+                    grants);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
             }
-
-            request.setAttribute("candidate_id", token.getSubject());
-            System.out.println(" ===== TOKEN =====");
-            System.out.println(token);
         }
 
         filterChain.doFilter(request, response);
