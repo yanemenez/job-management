484e605ddb6bc549bf2d63875019ef815b5641d2
Loyane Menezes <loyanesezenem5@gmail.com>
2025-11-12 18:05:43 -0300

    feat: enhance candidate module with Swagger docs and job listing endpoint

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,8 +1,19 @@
 package com.gestao.vagas.gestao_vagas.modules.candidate.controllers;
 
 import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateEntity;
+import com.gestao.vagas.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import com.gestao.vagas.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import com.gestao.vagas.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import com.gestao.vagas.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import com.gestao.vagas.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -23,6 +35,9 @@ public class CandidateController {
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
 
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
     @PostMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
     public  ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -35,7 +50,20 @@ public class CandidateController {
 
     }
     @GetMapping("/")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Perfil do candidato",
+            description = "Essa função é responsável por buscar as informações do perfil do candidato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(
+                            schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "User not found")
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> get(HttpServletRequest request) {
+
         var idCandidate = request.getAttribute("candidate_id");
         try {
             var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
@@ -43,6 +71,21 @@ public class CandidateController {
         }catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseadas no filtro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(
+                        array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 
 }
