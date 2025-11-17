b592a18afaa95cf3cb1f03520710b97bd14692d0
Loyane Menezes <loyanesezenem5@gmail.com>
2025-11-17 19:46:23 -0300

    chore: remove unused Mockito import

--- src/test/java/com/gestao/vagas/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/com/gestao/vagas/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -20,7 +20,6 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
