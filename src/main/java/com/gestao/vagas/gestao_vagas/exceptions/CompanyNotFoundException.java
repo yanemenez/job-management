294ac41f6d55357a3cda422306062d374d878282
Loyane Menezes <loyanesezenem5@gmail.com>
2025-11-17 19:38:30 -0300

    feat(exception): add custom CompanyNotFoundException

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/exceptions/CompanyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gestao.vagas.gestao_vagas.exceptions;
+
+public class CompanyNotFoundException extends RuntimeException{
+    public CompanyNotFoundException() {
+        super("Company not found");
+    }
+}
